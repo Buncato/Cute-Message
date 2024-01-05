5ccc697462e5618ba3640941054133eb3ae3eeb3
Rosella <113179063+Buncato@users.noreply.github.com>
2024-01-06 02:02:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const background = document.querySelector(".background");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you on Sunday!!!";
+  gif.src =
+    "https://gifdb.com/images/high/love-cute-blowing-hearts-sticker-dt47yz331dp4egtx.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
